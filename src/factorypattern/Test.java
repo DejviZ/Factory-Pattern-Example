d971f0991c2057a7013e2ef739075e9bd718d42c
Dejvi <dejvizyberi@unyt.edu.al>
2019-07-21 16:49:15 +0200

    Created the project

---
+++ src/factorypattern/Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorypattern;
+
+/**
+ *
+ * @author Dejvi
+ */
+public class Test {
+    public static void main(String[] args) {
+		Computer pc = ComputerFactory.getComputer("pc","2 GB","500 GB","2.4 GHz");
+		Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
+		System.out.println("Factory PC Config::"+pc);
+		System.out.println("Factory Server Config::"+server);
+	}
+}
